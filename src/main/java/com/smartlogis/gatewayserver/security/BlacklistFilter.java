cb95f35b6c9cca96a422745868f0fd7f81bdc58f
hyeon48165 <hyeon48615@gmail.com>
2025-11-23 15:54:49 +0900

    feat: 로그아웃 여부 확인 필터(2) 구현 #3

---
+++ src/main/java/com/smartlogis/gatewayserver/security/BlacklistFilter.java
@@ -0,0 +1,46 @@
+package com.smartlogis.gatewayserver.security;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.context.ReactiveSecurityContextHolder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import com.smartlogis.gatewayserver.auth.BlacklistService;
+
+import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Mono;
+
+@Component
+@RequiredArgsConstructor
+public class BlacklistFilter implements GlobalFilter, Ordered {
+
+	private final BlacklistService blacklistService;
+
+	@Override
+	public int getOrder() {
+		return 1;
+	}
+
+	@Override
+	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+		return ReactiveSecurityContextHolder.getContext()
+			.map(securityContext -> securityContext == null ? null : securityContext.getAuthentication())
+			.flatMap(auth -> {
+				if (!(auth instanceof JwtAuthenticationToken JwtAuth)) {
+					return chain.filter(exchange);
+				}
+
+				String tokenId = JwtAuth.getToken().getId();
+				if (blacklistService.isBlacklist(tokenId)) {
+					exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+					return exchange.getResponse().setComplete();
+				}
+
+				return chain.filter(exchange);
+			});
+	}
+}
